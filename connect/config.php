6b10ae0fa387634538ca7df63fc93d8d97a414c3
Jeferson Batista Souza <54870998+JefersonBatistaSouza@users.noreply.github.com>
2021-03-23 22:29:53 -0400

    Delete config.php

--- connect/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-//Dados de conexao ao Banco
-define('DB_HOST', '162.241.203.62');
-define('DB_USERNAME', 'comand28_dde');
-define('DB_PASSWORD', 'ifrodde');
-define('DB_DATABASE', 'comand28_DDE');
-
